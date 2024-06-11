f6d8fa46bddeab0f1247a334eac23fb8073511dd
carlos.santos-seisdedos <carlos.santos-seisdedos@renault.com>
2024-06-11 22:37:12 +0200

    added hidden redirect to stats site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,13 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://letesdev.github.io',
   base: '/portfolio',
+  redirects: {
+    '/stats': {
+      status: 302,
+      destination: 'https://librecounter.org/letesdev.github.io/show', 
+      permanent: true,
+    },
+  },
   // server: { host: true },
   integrations: [mdx(), tailwind()]
 });
